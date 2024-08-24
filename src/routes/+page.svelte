09db0fc172ca070e8525cf8e932d8a7a51906f9e
Jim <whoisjim@hotmail.com>
2024-08-24 15:11:40 +1000

    Selection and radius

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,7 +13,17 @@
   let fitToPage = true;
   let page;
 
+  let reference;
+
+  let radius = 100;
+
+  //let selected = undefined;
+  let selected = undefined;
   let analysis = [];
+
+  let doRender = () => {
+    console.log("Callback not yet installed");
+  };
 
   //https://en.wikipedia.org/wiki/Haversine_formula
   const hav = (theta) => {
@@ -105,7 +115,7 @@
         const referencePoints = annotations
           .filter((i) => i.contentsObj.str.startsWith("#techpdf"))
           .map((i) => [i.rect[0], i.rect[1]]);
-        console.log("reference points", referencePoints);
+        //console.log("reference points", referencePoints);
 
         const referenceVectors = annotations
           .filter((i) => i.contentsObj.str.startsWith("#techpdf"))
@@ -115,7 +125,12 @@
               .split(",")
               .map((j) => Number(j))
           );
-        console.log("reference vectors", referenceVectors);
+        //console.log("reference vectors", referenceVectors);
+
+        reference = {
+          points: referencePoints,
+          vectors: referenceVectors,
+        };
 
         analysis = annotations
           .filter((i) => !i.contentsObj.str.startsWith("#techpdf"))
@@ -127,6 +142,7 @@
                 referencePoints,
                 referenceVectors
               ),
+              rect: i.rect,
             };
           });
 
@@ -135,6 +151,10 @@
       });
     }
   }
+
+  $: {
+    console.log("analysis", analysis);
+  }
 </script>
 
 <svelte:head>
@@ -142,7 +162,7 @@
   <meta name="description" content="techpdf" />
 </svelte:head>
 
-<div>
+<div style="margin-bottom: 2em;">
   <a href="https://github.com/jimvella/techpdf"
     >https://github.com/jimvella/techpdf</a
   >
@@ -188,14 +208,101 @@ page <input type="number" bind:value={pageNumber} />
 
 {#if page}
   <div>
-    <PDFPage {page} style={fitToPage ? "width: 100%" : ""} />
+    <PDFPage
+      {page}
+      style={fitToPage ? "width: 100%" : ""}
+      afterRender={(context, canvas) => {
+        if (selected != undefined) {
+          console.log("selected", analysis[selected]);
+          console.log("page", page);
+
+          const viewport = page.getViewport();
+          const pdfWidth = viewport.viewBox[2];
+          const pdfHeight = viewport.viewBox[3];
+          const canvasWidth = canvas.width;
+          const canvasHeight = canvas.height;
+          const isOdd = (n) => {
+            return n % 2;
+          };
+          const pdfToCanvasCoords = (points) => {
+            // Canvas origin is top left,
+            // pdf origin is bottom left
+            let result = [];
+            for (let i = 0; i < points.length; i++) {
+              if (isOdd(i)) {
+                // y
+                result.push(
+                  ((pdfHeight - points[i]) * canvasHeight) / pdfHeight
+                );
+              } else {
+                // x cord
+                result.push((points[i] * canvasWidth) / pdfWidth);
+              }
+            }
+            return result;
+          };
+
+          let x = pdfToCanvasCoords(analysis[selected].rect);
+
+          // m to canvas units
+          // canvas distance between reference points
+          let a = pdfToCanvasCoords(reference.points[0]);
+          let b = pdfToCanvasCoords(reference.points[1]);
+          const canvasDistance = Math.sqrt(
+            Math.pow(b[0] - a[0], 2) + Math.pow(b[1] - a[1], 2)
+          );
+
+          // meters
+          const actualDistance = dist(
+            reference.vectors[0],
+            reference.vectors[1]
+          );
+
+          //context.fillStyle = "rgb(0,0,255,0.2)";
+          //context.fillRect(x[0], x[1], x[2] - x[0], x[3] - x[1]);
+
+          context.beginPath();
+          context.arc(
+            (x[0] + x[2]) / 2,
+            (x[1] + x[3]) / 2,
+            (radius / actualDistance) * canvasDistance,
+            0,
+            2 * Math.PI
+          );
+          context.fillStyle = "rgb(0,0,255,0.2)";
+          context.fill();
+          //context.lineWidth = 4;
+          //context.strokeStyle = "blue";
+          //context.stroke();
+        }
+      }}
+      doRenderCallback={(f) => (doRender = f)}
+    />
   </div>
 {/if}
 
+<div>
+  Radius (m) <input
+    type="number"
+    bind:value={radius}
+    on:change={() => {
+      doRender();
+    }}
+  />
+</div>
 <table>
   <tr><th>Description</th><th>Position</th></tr>
-  {#each analysis as i}
-    <tr>
+  {#each analysis as i, index}
+    <tr
+      on:click={() => {
+        selected = index;
+        console.log("calling");
+        doRender();
+      }}
+      style={selected != undefined && selected == index
+        ? "background-color: orange"
+        : ""}
+    >
       <td>{i.str}</td>
       <td>{i.pos.join(", ")}</td>
     </tr>
